ee6e753c4fc8414ece11b472905be69d1932a865
Matheus <teteuk6@gmail.com>
2026-07-02 09:15:11 -0300

    codeforces 4A solution

---
+++ code_forces/10_watermelon/wm_1.0.0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int w = 0;
+
+    scanf("%d", &w);
+    getchar();
+
+    if (1 <= w && w <= 100)
+    {
+        if (w > 2 && (w - 2) % 2 == 0)
+        { printf("YES\n"); }
+        else
+        { printf("NO\n"); }
+    }
+
+    return (0);
+}
