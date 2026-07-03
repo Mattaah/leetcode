59e9468a6b2d140ed42568a0d18e369f7b7e9757
Matheus <teteuk6@gmail.com>
2026-07-03 12:38:15 -0300

    codeforces 266A solution

---
+++ code_forces/13_stones_on_the_table/stones_1.0.0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *string = NULL;
+    int n = 0, count = 0;
+
+    scanf("%d", &n);
+    getchar();
+
+    string = (char *)malloc(n * sizeof(char));
+    if (string)
+    {
+        scanf("%s", string);
+        getchar();
+
+        for (int x = 1; x < n; x = x + 2)
+        {
+            if (string[x - 1] == string[x])
+            {
+                count = count + 1;
+            }
+            if (string[x + 1] == string[x])
+            {
+                count = count + 1;
+            }
+        }
+
+        printf("%d\n", count);
+    }
+
+    return (0);
+}
