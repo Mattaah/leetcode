6b6e36935ef2c6d91bc16487975858d4176c4b9b
Matheus <teteuk6@gmail.com>
2026-07-10 18:33:08 -0300

    codeforces 630A solution

---
+++ code_forces/29_again_twenty_five/atf.c
@@ -0,0 +1,18 @@
+/*
+    Codeforces 630A solution - v1.0.0 - 10/07/2026
+    Author: Mattah
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+
+    scanf("%d", &n);
+    getchar();
+
+    printf("%d\n", 25);
+
+    return (0);
+}
