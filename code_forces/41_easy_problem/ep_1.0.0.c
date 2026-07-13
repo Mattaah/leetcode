bc861174b74d94967055f80d805a74b5312fc432
Matheus <teteuk6@gmail.com>
2026-07-13 20:41:38 -0300

    codeforces 2044A solution

---
+++ code_forces/41_easy_problem/ep_1.0.0.c
@@ -0,0 +1,30 @@
+/*
+    Codeforces 2044A solution - v1.0.0 - 13/07/2026
+    Author: Mattah
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 0, value = 0, o_pair = 0;
+
+    scanf("%d", &n);
+    getchar();
+
+    for (int x = 0; x < n; x = x + 1)
+    {
+        scanf("%d", &value);
+        getchar();
+
+        o_pair = 0;
+        for (int y = 1; y < value; y = y + 1)
+        {
+            o_pair = o_pair + 1;
+        }
+
+        printf("%d\n", o_pair);
+    }
+
+    return (0);
+}
